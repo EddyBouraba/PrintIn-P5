c1b671d3db818e617b820225b868607d7e3221c7
EddyBouraba <142597566+EddyBouraba@users.noreply.github.com>
2023-08-25 09:12:07 +0200

    JS carrousel
    Initiation à JS grâce à l'ajout d'un carrousel

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+const dots = document.querySelector(".dots");
+const slidesImg = document.querySelector("#banner > img");
+const slidesText = document.querySelector("#banner > p");
+let currentIndex = 0;
+
+const switchSlide = () => {
+  slidesImg.src = `./assets/images/slideshow/${slides[currentIndex].image}`;
+  slidesText.innerHTML = slides[currentIndex].tagLine;
+};
+const toggleDot = (oldDot, newDot) => {
+  oldDot.classList.toggle("dot_selected");
+  newDot.classList.toggle("dot_selected");
+};
+
+leftArrow.addEventListener("click", () => {
+  //revenir sur backslide, modif aussi image + texte
+  const previousElement = dots.children.item(currentIndex);
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+    //revenir sur le dernier slide
+  }
+  const currentElement = dots.children.item(currentIndex);
+  toggleDot(previousElement, currentElement);
+  switchSlide();
+});
+rightArrow.addEventListener("click", () => {
+  const previousElement = dots.children.item(currentIndex);
+  currentIndex++;
+  if (currentIndex > slides.length - 1) {
+    currentIndex = 0;
+  }
+  //slide suivant avec changement d'img + text
+  const currentElement = dots.children.item(currentIndex);
+  toggleDot(previousElement, currentElement);
+  switchSlide();
+});
+slides.forEach((slide, index) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  if (currentIndex == index) {
+    dot.classList.add("dot_selected");
+  }
+  dots.appendChild(dot);
+  //Ajout des dot dans la div Dots
+});
